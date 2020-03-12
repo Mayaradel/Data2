cd3a4117d52fd85a3ea98194c23a671f0e0407de
20109 <20109@192.168.1.5>
2020-03-12 16:38:47 +0200

    lalaala

---
+++ SortingProject/src/SelectionSort.java
@@ -0,0 +1,22 @@
+
+public class SelectionSort {
+
+    void sort(int arr[]) {
+ 
+        int n = arr.length;
+ 
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int k = i + 1; k < n; k++) {
+                if (arr[k] < arr[min])
+                    min = k;
+            }
+ 
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+ 
+        }
+    }
+
+}
